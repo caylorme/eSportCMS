21df24f1d039453e91f3eb3bd7fb72a7b2839cb1
Jason Caylor <jason@caylor.me>
2017-08-04 02:20:43 -0400

    updated client with router and dynamic search functionality

--- src/client/script/app.js
+++ src/client/script/app.js
@@ -1,10 +1,22 @@
 Vue.use(VueMaterial)
-var signedIn = true;
+Vue.use(VueRouter)
+const Foo = { template: '<div>This is some test stuff for Foo. <b>TESTING!</b></div>' }
+const Bar = { template: '<div>This is more test stuff, but for Bar. It is really great and I like it</div>' }
+const routes = [
+    { name: "foo", icon: "help", path: '/foo', component: Foo },
+    { name: "bar", icon: "grade", path: '/bar', component: Bar }
+]
+const router = new VueRouter({
+    routes: routes
+})
 var App = new Vue({
     el: '#app',
-    methods: {
-        toggleLeftSidenav() {
-            this.$refs.leftSidenav.toggle();
-        }
-    }
+    data: {
+        signedIn: false,
+        routes: routes,
+        show: false
+    },
+    router,
+    methods: {},
+    watch: {}
 })
